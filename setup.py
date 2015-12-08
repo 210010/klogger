10438b0a82981753727a783cce6be7d51fc04ed9
kaniblu <kaniblurous@gmail.com>
2015-12-08 16:06:02 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="logjob", version="0.1", description="Time and log jobs.", author="Kang Min Yoo", author_email="kaniblurous@gmail.com", py_modules=["logjob"])
